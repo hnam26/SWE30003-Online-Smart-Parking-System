6dd223313bec50f7b168cb6281a59529fbd0caea
hnam26 <hoainamkin03@gmail.com>
2024-08-05 14:39:48 +0700

    update code

--- DataAccessLayer/models/personal/booking.py
+++ DataAccessLayer/models/personal/booking.py
@@ -1,8 +1,17 @@
-from sqlalchemy import Column, Integer, ForeignKey, DateTime, DECIMAL
+import enum
+
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, DECIMAL, Enum
 from sqlalchemy.orm import relationship
-from DataAccessLayer.models.base import Base
-from typing import TYPE_CHECKING
-from DataAccessLayer.models.user import User
+from .base import Base
+from enum import Enum as pythonEnum
+
+
+class BookingStatus(pythonEnum):
+    PENDING = 'PENDING'
+    PAID = 'PAID'
+    IN = 'IN'
+    OUT = 'OUT'
+    CANCELLED = 'CANCELLED'
 
 class Booking(Base):
     __tablename__ = 'Booking'
@@ -12,6 +21,7 @@ class Booking(Base):
     __parkingSlotId = Column("parking_slot_id", Integer, ForeignKey('ParkingSlot.parking_slot_id'), nullable=False)
     __startTime = Column("start_time", DateTime, nullable=False)
     __duration = Column("duration", DECIMAL(2, 1), nullable=False)
+    __status = Column("status", Enum('PENDING', 'PAID', 'IN', 'OUT', 'CANCELLED'), nullable=False)
 
     __user = relationship('User', back_populates='bookings')
     __vehicle = relationship('Vehicle', back_populates='bookings')
@@ -37,3 +47,11 @@ class Booking(Base):
     @property
     def getDuration(self):
         return self.__duration
+
+    @property
+    def getStatus(self):
+        return self.__status
+
+    @getStatus.setter
+    def setStatus(self, status : BookingStatus):
+        self.__status = status
--- DataAccessLayer/models/personal/user.py
+++ DataAccessLayer/models/personal/user.py
@@ -1,104 +1,87 @@
 import re
+from sqlalchemy import Column, Integer, String, Date, CheckConstraint
+from sqlalchemy.orm import relationship
+from .base import Base
 from datetime import datetime
-from typing import List, Union, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    pass
 
 
-class User:
-    def __init__(self, username: str, password: str, firstName: str, lastName: str, email: str, phone: str, dob: str,
-                 vehicle: List['Vehicle'] = None, booking: List[Union['Booking', None]] = None,
-                 payment: 'Payment' = None):
+class User(Base):
+    __tablename__ = 'User'
+    __userId = Column("user_id", Integer, primary_key=True, autoincrement=True)
+    __username = Column("username", String(100), nullable=False)
+    __password = Column("password", String(60), nullable=False)
+    __firstName = Column("first_name", String(100), nullable=False)
+    __lastName = Column("last_name", String(100), nullable=False)
+    __email = Column("email", String(100), unique=True, nullable=False)
+    __phone = Column("phone", String(10), nullable=False)
+    __dob = Column("dob", Date, nullable=False)
+
+    __vehicles = relationship('Vehicle', back_populates='user')
+    __bookings = relationship('Booking', back_populates='user')
+    __payments = relationship('Payment', back_populates='user')
+
+    __table_args__ = (
+        CheckConstraint(
+            'email ~* \'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$\'',
+            name='check_email_format'
+        ),
+        CheckConstraint(
+            'phone ~* \'^\d{10}$\'',
+            name='check_phone_format'
+        ),
+        CheckConstraint(
+            'dob <= CURRENT_DATE',
+            name='check_dob_past'
+        ),
+    )
+
+    def __init__(self, email, phone, dob, username, password, firstName, lastName):
+        self.__email = self.validateEmail(email)
+        self.__phone = self.validatePhone(phone)
+        self.__dob = self.validateDob(dob)
         self.__username = username
         self.__password = password
         self.__firstName = firstName
         self.__lastName = lastName
-        self.__email = self.validateEmail(email)
-        self.__phone = self.validatePhone(phone)
-        self.__dob = self.validateDob(dob)
-        self.__vehicle = vehicle
-        self.__booking = booking
-        self.__payment = payment
 
     @staticmethod
-    def validateEmail(email: str) -> str:
+    def validateEmail(email):
         email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         if not re.match(email_regex, email):
             raise ValueError("Invalid email address")
         return email
 
     @staticmethod
-    def validatePhone(phone: str) -> str:
+    def validatePhone(phone):
         phone_regex = r'^\d{10}$'
         if not re.match(phone_regex, phone):
             raise ValueError("Invalid phone number")
         return phone
 
     @staticmethod
-    def validateDob(dob: str) -> str:
+    def validateDob(dob):
         dob_regex = r'^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\d{4})$'
         if not re.match(dob_regex, dob.strip()):
             raise ValueError("Invalid date of birth")
         date = datetime.strptime(dob, "%d-%m-%Y").date()
-        return date.strftime("%Y-%m-%d")
+        return date
 
-    def getUsername(self) -> str:
+    @property
+    def getVehicle(self):
+        return self.__vehicles
+
+    @property
+    def getBooking(self):
+        return self.__bookings
+
+    @property
+    def getPayment(self):
+        return self.__payments
+
+    @property
+    def getUsername(self):
         return self.__username
 
-    def setUsername(self, username: str):
-        self.__username = username
-
-    def getPassword(self) -> str:
+    @property
+    def getPassword(self):
         return self.__password
-
-    def setPassword(self, password: str):
-        self.__password = password
-
-    def getFirstName(self) -> str:
-        return self.__firstName
-
-    def setFirstName(self, firstName: str):
-        self.__firstName = firstName
-
-    def getLastName(self) -> str:
-        return self.__lastName
-
-    def setLastName(self, lastName: str):
-        self.__lastName = lastName
-
-    def getFullName(self) -> str:
-        return f"{self.__firstName} {self.__lastName}"
-
-    def getEmail(self) -> str:
-        return self.__email
-
-    def setEmail(self, email: str):
-        self.__email = self.validateEmail(email)
-
-    def getPhone(self) -> str:
-        return self.__phone
-
-    def setPhone(self, phone: str):
-        self.__phone = self.validatePhone(phone)
-
-    def getDob(self) -> str:
-        return self.__dob
-
-    def setDob(self, dob: str):
-        self.__dob = self.validateDob(dob)
-
-    def getVehicles(self) -> List['Vehicle']:
-        return self.__vehicle
-
-    def addVehicle(self, vehicle: 'Vehicle'):
-        self.__vehicle.append(vehicle)
-
-    def getBookings(self) -> List[Union['Booking', None]]:
-        return self.__booking
-
-    def addBooking(self, booking: 'Booking'):
-        self.__booking.append(booking)
-
-    def getPaymentMethod(self) -> 'Payment':
-        return self.__payment
