d9facd2352444b71df1c3c566250f9b2757214b2
hnam26 <hoainamkin03@gmail.com>
2024-08-05 09:35:41 +0700

    update booking

--- DataAccessLayer/models/booking.py
+++ DataAccessLayer/models/booking.py
@@ -2,23 +2,38 @@ from sqlalchemy import Column, Integer, ForeignKey, DateTime, DECIMAL
 from sqlalchemy.orm import relationship
 from .base import Base
 from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from .user import User
-    from .vehicle import Vehicle
-    from .parkingSlot import ParkingSlot
-    from .payment import Payment
-
+from .user import User
 
 class Booking(Base):
     __tablename__ = 'Booking'
-    booking_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('User.user_id'), nullable=False)
-    vehicle_id = Column(Integer, ForeignKey('Vehicle.vehicle_id'), nullable=False)
-    parking_slot_id = Column(Integer, ForeignKey('ParkingSlot.parking_slot_id'), nullable=False)
-    start_time = Column(DateTime, nullable=False)
-    duration = Column(DECIMAL(2, 1), nullable=False)
-    user = relationship('User', back_populates='bookings')
-    vehicle = relationship('Vehicle', back_populates='bookings')
-    parking_slot = relationship('ParkingSlot', back_populates='bookings')
-    payment = relationship('Payment', uselist=False, back_populates='booking')
+    __bookingId = Column("booking_id", Integer, primary_key=True, autoincrement=True)
+    __userId = Column("user_id", Integer, ForeignKey('User.user_id'), nullable=False)
+    __vehicleId = Column("vehicle_id", Integer, ForeignKey('Vehicle.vehicle_id'), nullable=False)
+    __parkingSlotId = Column("parking_slot_id", Integer, ForeignKey('ParkingSlot.parking_slot_id'), nullable=False)
+    __startTime = Column("start_time", DateTime, nullable=False)
+    __duration = Column("duration", DECIMAL(2, 1), nullable=False)
+
+    __user = relationship('User', back_populates='bookings')
+    __vehicle = relationship('Vehicle', back_populates='bookings')
+    __parkingSlot = relationship('ParkingSlot', back_populates='bookings')
+    __payment = relationship('Payment', uselist=False, back_populates='booking')
+
+    @property
+    def getUser(self):
+        return self.__user
+
+    @property
+    def getVehicle(self):
+        return self.__vehicle
+
+    @property
+    def getParkingSlot(self):
+        return self.__parkingSlot
+
+    @property
+    def getStartTime(self):
+        return self.__startTime
+
+    @property
+    def getDuration(self):
+        return self.__duration
